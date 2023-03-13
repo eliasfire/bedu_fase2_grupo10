5d05d6e51a4d57f68314657e85604090e7659f76
Anna Luz Gómez <110350110+seaurchin-miau@users.noreply.github.com>
2023-03-12 18:15:01 -0600

    Update Person.java

--- Sesion 4/Postwork/Person.java
+++ Sesion 4/Postwork/Person.java
@@ -1,1 +1,39 @@
+package org.bedu.java.backend.postwork4.datos;
 
+public class Person {
+
+    private String name;
+    private String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Person() {  //Constructor sin parámetros
+
+    }
+    public Person(String name, String phone) {  //Constructor con parámetros
+        this.name = name;
+        this.name = phone;
+    }
+
+    @Override //Sobreescribir para mostrar los datos necesarios
+    public String toString() {
+        return "Persona{" +
+                "nombre ='" + name + '\'' +
+                ", numero de telefono ='" + phone + '\'' +
+                '}';
+    }
+}
