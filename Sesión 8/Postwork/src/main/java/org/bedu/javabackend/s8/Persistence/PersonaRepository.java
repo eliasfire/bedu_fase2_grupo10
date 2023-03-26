aecfc380a0fec5e3bad60ff427696c0985609782
eliasfire <vakito85@gmail.com>
2023-03-26 18:41:18 -0300

    refactoring names

--- Sesión 8/Postwork/src/main/java/org/bedu/javabackend/s8/Persistence/PersonaRepository.java
+++
@@ -1,8 +0,0 @@
-package org.bedu.java.backend.sesion8.persistence;
-
-
-import org.bedu.java.backend.sesion8.model.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface PersonaRepository extends JpaRepository<Persona, Long> {
-
-}
